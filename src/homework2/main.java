f2278e2087426921832c083c1eb81f0239b8aef1
sesan <sesan@DESKTOP-M8TTR56>
2020-10-29 22:33:51 +0900

    commit

---
+++ src/homework2/main.java
@@ -0,0 +1,135 @@
+package homework2;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 어떤 대학의 정보제공 앱 제작 - 회원가입 가능
+		// 회원종류 : 일반회원, 학생, 교수, 근로학생
+
+		// 1. 일반회원 : 이름, 나이
+		// 2. 학 생 : 이름, 나이, 전공
+		// 3. 교 수 : 이름, 나이, 연구과제
+		// 4. 근로학생 : 이름, 나이, 전공, 업무
+
+		// 일반회원
+		Person p = new Person("sam", 20);
+		p.show();
+
+		// 학생
+		Student stu = new Student("robin", 25, "android");
+		stu.show();
+
+		// 교수
+		Professor pro = new Professor("lee", 50, "mobile optimization");
+		pro.show();
+
+		// 근로학생
+		AlbaStudent alba = new AlbaStudent("hong", 23, "web", "pc management");
+		alba.show();
+
+	}
+
+
+}
+
+  class Person {
+
+	private String name;
+	private int age;
+
+	// 생성자 메소드 : 객체생성(new)될때 자동으로 실행되는 메소드
+	public Person() {
+		name = "no name";
+		age = 0;
+	}
+
+	// 생성자 오버로딩
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public void show() {
+		System.out.println("name : " + name);
+		System.out.println("age : " + age);
+	}
+
+}
+
+
+ class Student extends Person{
+	
+	//이미 name, age를 보유한 상태!!
+	private String major; //전공
+	
+	public Student() {
+		super();
+		major="";
+	}
+	
+	public Student(String name, int age, String major) {
+		super(name, age);
+		this.major= major;		
+	}
+	
+	//오버라이드
+	public void show() {
+		super.show();
+		System.out.println("major : "+ major);
+	}
+	
+
+}
+ 
+
+
+class Professor extends Person{
+	
+	private String subject="";
+	
+	public Professor() {
+		// TODO Auto-generated constructor stub		
+	}
+	
+	//생성자 오버로딩
+	public Professor(String name, int age, String subject) {
+		// TODO Auto-generated constructor stub		
+		super(name, age);
+		this.subject= subject;
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		super.show();
+		System.out.println("subject : " + subject);
+	}
+	
+
+}
+class AlbaStudent extends Student {
+	
+	private String task=new String("");
+
+	public AlbaStudent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlbaStudent(String name, int age, String major, String task) {
+		super(name, age, major);
+		// TODO Auto-generated constructor stub
+		this.task= task;
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		super.show();
+		System.out.println("task : "+ task);
+	}
+}
+
+
