d574679bf189be38f7cfa2d4eeed7f61f8a8d6ac
sesan <sesan@DESKTOP-M8TTR56>
2020-11-01 00:36:03 +0900

    commit

--- src/homework2/main.java
+++ src/homework2/main.java
@@ -1,6 +1,5 @@
 package homework2;
 
-
 public class main {
 
 	public static void main(String[] args) {
@@ -11,8 +10,11 @@ public class main {
 
 		// 1. 일반회원 : 이름, 나이
 		// 2. 학 생 : 이름, 나이, 전공
-		// 3. 교 수 : 이름, 나이, 연구과제
-		// 4. 근로학생 : 이름, 나이, 전공, 업무
+		// 3. 학 생 2 : 이름, 나이, 전공		
+		// 4. 교 수 : 이름, 나이, 연구과제
+		// 5. 근로학생 : 이름, 나이, 전공, 업무
+		// 6. 노동자 : 이름, 나이, 업무
+		// 7. 깡패 : 이름,나이, 총 소지여부
 
 		// 일반회원
 		Person p = new Person("sam", 20);
@@ -21,6 +23,9 @@ public class main {
 		// 학생
 		Student stu = new Student("robin", 25, "android");
 		stu.show();
+		
+		Student2 stu2 = new Student2("kevin",25,"phython");
+		stu2.show();
 
 		// 교수
 		Professor pro = new Professor("lee", 50, "mobile optimization");
@@ -30,12 +35,19 @@ public class main {
 		AlbaStudent alba = new AlbaStudent("hong", 23, "web", "pc management");
 		alba.show();
 
-	}
+		//노동자
+		worker worker = new worker("kim", 25, "worksomething");
+		worker.show();
+		
+		//깡패
+		gangster gangster = new gangster("kim",25,"have");
+		gangster.show();
 
+	}
 
 }
 
-  class Person {
+class Person {
 
 	private String name;
 	private int age;
@@ -59,60 +71,79 @@ public class main {
 
 }
 
+class Student extends Person {
 
- class Student extends Person{
-	
-	//이미 name, age를 보유한 상태!!
-	private String major; //전공
-	
+	// 이미 name, age를 보유한 상태!!
+	private String major; // 전공
+
 	public Student() {
 		super();
-		major="";
+		major = "";
 	}
-	
+
 	public Student(String name, int age, String major) {
 		super(name, age);
-		this.major= major;		
+		this.major = major;
 	}
-	
-	//오버라이드
+
+	// 오버라이드
 	public void show() {
 		super.show();
-		System.out.println("major : "+ major);
+		System.out.println("major : " + major);
 	}
-	
 
 }
- 
+class Student2 extends Person {
 
+	// 이미 name, age를 보유한 상태!!
+	private String major; // 전공
 
-class Professor extends Person{
-	
-	private String subject="";
-	
-	public Professor() {
-		// TODO Auto-generated constructor stub		
+	public Student2() {
+		super();
+		major = "";
 	}
-	
-	//생성자 오버로딩
-	public Professor(String name, int age, String subject) {
-		// TODO Auto-generated constructor stub		
+
+	public Student2(String name, int age, String major) {
 		super(name, age);
-		this.subject= subject;
+		this.major = major;
 	}
-	
+
+	// 오버라이드
+	public void show() {
+		super.show();
+		System.out.println("major : " + major);
+	}
+
+}
+
+
+class Professor extends Person {
+
+	private String subject = "";
+
+	public Professor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	// 생성자 오버로딩
+	public Professor(String name, int age, String subject) {
+		// TODO Auto-generated constructor stub
+		super(name, age);
+		this.subject = subject;
+	}
+
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
 		super.show();
 		System.out.println("subject : " + subject);
 	}
-	
 
 }
+
 class AlbaStudent extends Student {
-	
-	private String task=new String("");
+
+	private String task = new String("");
 
 	public AlbaStudent() {
 		// TODO Auto-generated constructor stub
@@ -121,15 +152,59 @@ class AlbaStudent extends Student {
 	public AlbaStudent(String name, int age, String major, String task) {
 		super(name, age, major);
 		// TODO Auto-generated constructor stub
-		this.task= task;
+		this.task = task;
 	}
-	
+
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
 		super.show();
-		System.out.println("task : "+ task);
+		System.out.println("task : " + task);
 	}
+
+}
+
+class worker extends Person {
+
+	private String job; // 일
+
+	public worker() {
+		super();
+		job = "";
+	}
+
+	public worker(String name, int age, String job) {
+		super(name, age);
+		this.job = job;
+	}
+
+	// 오버라이드
+	public void show() {
+		super.show();
+		System.out.println("job : " + job);
+	}
+
+}
+class gangster extends Person {
+
+	private String gunhave; // 일
+
+	public gangster() {
+		super();
+		gunhave = "";
+	}
+
+	public gangster(String name, int age, String gunhave) {
+		super(name, age);
+		this.gunhave = gunhave;
+	}
+
+	// 오버라이드
+	public void show() {
+		super.show();
+		System.out.println("gunhave : " + gunhave);
+	}
+
 }
 
 
